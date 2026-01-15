45210c6d5f4749758239d547915b56700ee8c608
Jake Jung <magoja@gmail.com>
2026-01-14 16:17:29 -0800

    Fix clicking

--- mtg-limited/list.js
+++ mtg-limited/list.js
@@ -129,10 +129,14 @@ function renderCards(cards) {
     cardElement.appendChild(img);
 
     if (card.is_transform) {
+      cardElement.classList.add('transformable');
+
       const flipBtn = document.createElement('button');
       flipBtn.classList.add('flip-button');
       flipBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 4V1L8 5l4 4V6c3.31 0 6 2.69 6 6 0 1.01-.25 1.97-.7 2.8l1.46 1.46C19.54 15.03 20 13.57 20 12c0-4.42-3.58-8-8-8zm0 14c-3.31 0-6-2.69-6-6 0-1.01.25-1.97.7-2.8L5.24 7.74C4.46 8.97 4 10.43 4 12c0 4.42 3.58 8 8 8v3l4-4-4-4v3z"/></svg>`;
-      flipBtn.onclick = () => {
+
+      // Make whole card clickable for transform cards
+      cardElement.onclick = () => {
         currentFaceIndex = (currentFaceIndex + 1) % card.faces.length;
         const newFace = card.faces[currentFaceIndex];
         if (newFace.image_uris) {
@@ -140,6 +144,7 @@ function renderCards(cards) {
           img.alt = newFace.name;
         }
       };
+
       cardElement.appendChild(flipBtn);
     }
 
